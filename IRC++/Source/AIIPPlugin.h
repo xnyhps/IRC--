d75c2788d1ed50020dba72a907fa1e28b9f8976a
Thijs Alkemade <thijsalkemade@gmail.com>
2011-12-04 12:28:35 +0100

    Started the probject.

---
+++ IRC++/Source/AIIPPlugin.h
@@ -0,0 +1,13 @@
+//
+//  AIIPPlugin.h
+//  IRC++
+//
+//  Created by Thijs Alkemade on 04-12-11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+
+
+@interface AIIPPlugin : AIPlugin
+
+@end
---
+++ IRC++/Source/AIIPPlugin.m
@@ -0,0 +1,18 @@
+//
+//  AIIPPlugin.m
+//  IRC++
+//
+//  Created by Thijs Alkemade on 04-12-11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "AIIPPlugin.h"
+
+@implementation AIIPPlugin
+
+- (void)installPlugin
+{
+	
+}
+
+@end
